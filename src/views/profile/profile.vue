084ead0d9470125c64d63ae09c375233972ddc1f
GpjScy <jiacheng12@outlook.com>
2020-08-05 22:14:08 +0800

    '初始化'

---
+++ src/views/profile/profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h2>
+        Profile
+      </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "profile"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/users/users.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h2>
+        Users
+      </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "users"
+    }
+</script>
+
+<style scoped>
+
+</style>
